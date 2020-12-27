352442ccf08361869c4a7e64830fb82ac44d1989
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2020-12-27 19:22:43 -0300

    mvp project started (based on unity tutorial)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float turnSpeed;
+
+    [SerializeField] float acceleration;
+    [SerializeField] float control;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        control = Input.GetAxis("Horizontal");
+        acceleration = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * acceleration);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * control);
+    }
+}
